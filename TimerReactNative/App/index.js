19a1f2b7edb82ba896aefbeea6db1f8fde3b7b64
kent28808 <kktanoue@gmail.com>
2022-03-15 23:33:34 -0700

    added formatNumber to diplay correct time format

--- TimerReactNative/App/index.js
+++ TimerReactNative/App/index.js
@@ -6,7 +6,7 @@ const screen = Dimensions.get('window');
 const getRemaining = (time) => {
     const mins = Math.floor(time / 60);
     const secs = time - mins * 60;
-    return { mins, secs };
+    return { mins: formatNumber(mins), secs: formatNumber(secs) };
 }
 
 export default function App() {
